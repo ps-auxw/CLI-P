21bd35cdddad4aac4d638cdf9c6ad0dfa4730c23
ps-auxw <ps-auxw@users.noreply.github.com>
2021-03-07 17:52:03 +0100

    Revert accidential revert of setup.sh changes

--- setup.sh
+++ setup.sh
@@ -43,6 +43,7 @@ echo iglovikov-helper-functions==0.0.53 > requirements.txt
 python setup.py install
 
 # Download InsightFace-v2 ArcFace model
-mkdir -p ~/.cache/InsightFace-v2
-wget -c 'https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar' -O ~/.cache/InsightFace-v2/BEST_checkpoint_r101.tar
-sha256sum -c <<< "c32fab2e978b25def0c201b5b5948e7068944d113438105b5b291f145393c95a  $HOME/.cache/InsightFace-v2/BEST_checkpoint_r101.tar"
+DOWNLOAD_CACHE_DIR="${XDG_CACHE_HOME-$HOME/.cache}/InsightFace-v2"
+mkdir -p "$DOWNLOAD_CACHE_DIR"
+wget -c 'https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar' -O "$DOWNLOAD_CACHE_DIR/BEST_checkpoint_r101.tar"
+sha256sum -c <<< "c32fab2e978b25def0c201b5b5948e7068944d113438105b5b291f145393c95a  $DOWNLOAD_CACHE_DIR/BEST_checkpoint_r101.tar"
