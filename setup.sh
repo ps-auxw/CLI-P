8c5dd064c82a4a1d5273eec78d4b50be26d43b41
ps-auxw <ps-auxw@users.noreply.github.com>
2021-03-07 17:10:33 +0100

    Fix and clean up setup.sh
    Thanks to @canvon for the bash variant

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ echo iglovikov-helper-functions==0.0.53 > requirements.txt
 python setup.py install
 
 # Download InsightFace-v2 ArcFace model
-mkdir -p ~/.cache/InsightFace-v2
-CHECKPOINT_PATH="$(python -c "$(echo -e "import xdg.BaseDirectory\nimport os\nprint(os.path.join(xdg.BaseDirectory.xdg_cache_home, 'InsightFace-v2', 'BEST_checkpoint_r101.tar'))")")"
-wget -c 'https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar' -O "$CHECKPOINT_PATH"
-sha256sum -c <<< "c32fab2e978b25def0c201b5b5948e7068944d113438105b5b291f145393c95a  $CHECKPOINT_PATH"
+DOWNLOAD_CACHE_DIR="${XDG_CACHE_HOME-$HOME/.cache}/InsightFace-v2"
+mkdir -p "$DOWNLOAD_CACHE_DIR"
+wget -c 'https://github.com/foamliu/InsightFace-v2/releases/download/v1.0/BEST_checkpoint_r101.tar' -O "$DOWNLOAD_CACHE_DIR/BEST_checkpoint_r101.tar"
+sha256sum -c <<< "c32fab2e978b25def0c201b5b5948e7068944d113438105b5b291f145393c95a  $DOWNLOAD_CACHE_DIR/BEST_checkpoint_r101.tar"
