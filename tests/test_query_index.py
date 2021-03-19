b307d0947be89966dc5e78719fa37200a085edc1
Fabian Pietsch <fabian@canvon.de>
2021-03-19 04:38:39 +0100

    tests: Generalize verify_command_behaviour_setfield() from test_command_ft()
    As the code now is reuseable, it should now be easier/possible-at-all
    to add further test cases for more such commands.

--- tests/test_query_index.py
+++ tests/test_query_index.py
@@ -19,52 +19,59 @@ class TestQueryIndex(unittest.TestCase):
                 ret = code()
             return f.getvalue(), ret
 
-    @classmethod
-    def verify_line_structure(cls, line):
+    def verify_line_structure(self, line, msg=None):
+        msg_suffix = "no line structure" + ('' if msg is None else ' : ' + msg)
         if line is None:
-            return False
+            self.fail("line is None => " + msg_suffix)
         if not line.endswith("\n"):
-            return False
-        return True
+            self.fail("line doesn't end with newline => " + msg_suffix)
 
-    @classmethod
-    def verify_singleline_structure(cls, line):
-        if not cls.verify_line_structure(line):
-            return False
-        if line.count("\n") != 1:
-            return False
-        return True
+    def verify_singleline_structure(self, line, msg=None):
+        msg_suffix = "no singleline structure" + ('' if msg is None else ' : ' + msg)
+        self.verify_line_structure(line, msg_suffix)
+        count = line.count("\n")
+        if count != 1:
+            self.fail(f"{count} newlines instead of 1 => " + msg_suffix)
+
+    def verify_command_behaviour_setfield(self, command_name, field_name,
+        value, expect_success, success_msg_prefix=None, fail_msg_prefix=None,
+        expect_iterationDone=True, expect_singleline_structure=True):
+        search = self.search
+        setattr(search, field_name, None)
+        search.in_text = f"{command_name} {value}"
+        msg_suffix = f"failed at command {command_name!r} data point {value} expect {'success' if expect_success else 'fail'}"
+
+        output, iterationDone = self.capture_stdout(search.do_command)
+        if expect_iterationDone is not None:
+            self.assertEqual(expect_iterationDone, iterationDone, msg=f"Command {'did' if iterationDone else 'didnt'} request 'no search'. => {msg_suffix}")
+
+        if expect_singleline_structure is not None:
+            if not expect_singleline_structure:
+                raise NotImplementedError("expect_singleline_structure=False not implemented => " + msg_suffix)
+            self.verify_singleline_structure(output, msg=f"Command didn't return exactly a single line of output: {output!r} => {msg_suffix}")
+
+        if expect_success is None:
+            return
+        expect_msg_prefix = success_msg_prefix if expect_success else fail_msg_prefix
+        if expect_msg_prefix is not None:
+            if not output.startswith(expect_msg_prefix):
+                self.assertEqual(expect_msg_prefix, output, msg="Output message didn't start with expected message prefix. => " + msg_suffix)
+
+        stored_value = getattr(search, field_name)
+        if expect_success:
+            self.assertEqual(value, stored_value, msg="Command didn't update Search object to data point value. => " + msg_suffix)
+        else:
+            self.assertIsNone(stored_value, msg="Command updated Search object although it shouldn't have. => " + msg_suffix)
 
     def test_command_ft(self):
-        search = self.search
-        success_msg_prefix = "Set face similarity threshold"
-        fail_msg_prefix = "Invalid face threshold"
-
-        def verify(value, expect_success):
-            search.face_threshold = None
-            search.in_text = f"ft {value}"
-            assert_msg_prefix = f"data point {value}, expecting {'success' if expect_success else 'fail'}"
-
-            output, iterationDone = self.capture_stdout(search.do_command)
-            self.assertTrue(iterationDone, msg=assert_msg_prefix + ": Command didn't request 'no search'.")
-
-            self.assertTrue(self.verify_singleline_structure(output), msg=assert_msg_prefix + ": Command didn't return exactly a single line of output: {repr(output)}")
-
-            expect_msg_prefix = success_msg_prefix if expect_success else fail_msg_prefix
-            if not output.startswith(expect_msg_prefix):
-                self.assertEqual(expect_msg_prefix, output, msg=assert_msg_prefix + ": Output message didn't start with expected message prefix.")
-
-            if expect_success:
-                self.assertEqual(value, search.face_threshold, msg=assert_msg_prefix + ": Command didn't update Search object to data point value.")
-            else:
-                self.assertIsNone(search.face_threshold, msg=assert_msg_prefix + ": Command updated Search object although it shouldn't have.")
-
         values = [
             (1.0, True), (0.5, True), (0.0, True),
             (1.1, False), (-0.1, False),
         ]
         for v in values:
-            verify(*v)
+            self.verify_command_behaviour_setfield("ft", "face_threshold", v[0],  v[1],
+                success_msg_prefix="Set face similarity threshold",
+                fail_msg_prefix="Invalid face threshold")
 
 if __name__ == '__main__':
     unittest.main()
